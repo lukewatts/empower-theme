dd4d5eb7067185abfb7aa9548c5a35b1c4e047c2
Luke Watts <luke@luke-watts.com>
2017-05-06 17:10:03 +0100

    Moving first half of style.css into partials

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const path 			= require('path');
+const gulp 			= require('gulp');
+const sass 			= require('gulp-sass');
+const sourcemaps 	= require('gulp-sourcemaps');
+
+var path_to = {
+	src: {
+		sass: path.join(__dirname, 'src', 'sass')
+	},
+	dist: {
+		css: path.join(__dirname, 'css')
+	}
+};
+
+gulp.task('default', ['css:dist']);
+
+gulp.task('css:watch', () => {
+	gulp.watch([path_to.src.sass + '/**/*.scss', path_to.src.sass + '/**/*.sass'], ['sass']);
+});
+
+gulp.task('css:dev', () => {
+	return gulp.src([path_to.src.sass + '/**/*.scss', path_to.src.sass + '/**/*.sass'])
+    	.pipe(sass().on('error', sass.logError))
+    	.pipe(gulp.dest(path_to.dist.css));
+});
+
+gulp.task('css:dist', () => {
+	return gulp.src([path_to.src.sass + '/**/*.scss', path_to.src.sass + '/**/*.sass'])
+    	.pipe(sass({
+    		outputStyle: 'compressed'
+    	}).on('error', sass.logError))
+    	.pipe(gulp.dest(path_to.dist.css));
+});
